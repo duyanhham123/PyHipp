7e1b5e279848d51aefcf599ebb903dbecc3c2928
AWS ParallelCluster user <ec2-user@ip-10-0-3-104.ap-southeast-1.compute.internal>
2021-11-08 11:07:06 +0000

    some minor fix

--- freq-slurm.sh
+++ freq-slurm.sh
@@ -16,6 +16,6 @@
 python -u -c "import PyHipp as pyh; \
 import time; \
 pyh.FreqSpectrum(saveLevel=1); \
-pyh.FreqSpectrum(loadHighPass=True, pointsPerWindow=3000, saveLevel=1);
+pyh.FreqSpectrum(loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
 print(time.localtime());"
 
